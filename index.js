62aa00106409200a41016107e1ea2bf71f638454
kogotti@yahoo.com <kogotti@yahoo.com>
2018-08-01 19:14:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ dodger.style.bottom = '0px'
 
 document.addEventListener('keydown', function(e){
   if (e.which === 39) {
-   moveDodgerRight(
+   moveDodgerRight()
    var rightNumbers = dodger.style.right.replace('px', '');
     var right = parseInt(rightNumbers, 10);
     if (right > 0){
     dodger.style.right = `${right - 1}px`;
-   ) }
+    }
 }
 })
