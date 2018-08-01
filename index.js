c6a63f639910dad42f0f8d22f2fe1f27680aba9b
kogotti@yahoo.com <kogotti@yahoo.com>
2018-08-01 18:56:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var dodger = document.getElementById('dodger')
 dodger.style.backgroundColor = 'red'
 dodger.style.bottom = '0px'
 
-document.addEventListener('keydown', function(){
-  if (e.which === 39) {
+/*document.addEventListener('keydown', function(){
+  if (e.which === 39) {*/
    var rightNumbers = dodger.style.right.replace('px', '');
     var right = parseInt(rightNumbers, 10);
     if (right > 0){
